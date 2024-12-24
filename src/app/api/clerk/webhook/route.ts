bb152f92f506cb511af625a7c76bbc2a21cc948c
RRFayad <renan.r.fayad@gmail.com>
2024-12-24 09:21:35 -0300

    fix(auth): Add error handling in create user route

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,18 +1,33 @@
 import { db } from "~/server/db";
+import { User } from "@prisma/client";
 
 export const POST = async (req: Request) => {
   const { data } = await req.json();
 
   const id = data.id;
   const emailAddress = data.email_addresses[0].email_address;
-  const firstName = data.first_name;
-  const lastName = data.last_name;
-  const imageUrl = data.image_url;
+  const firstName = data.first_name || "";
+  const lastName = data.last_name || "";
+  const imageUrl = data.image_url || "";
 
-  const user = await db.user.create({
-    data: { email: emailAddress, firstName, lastName, id, imageUrl },
-  });
+  const toBeCreatedUserData: User = {
+    email: emailAddress,
+    firstName,
+    lastName,
+    id,
+    imageUrl,
+  };
 
-  console.log(user);
+  try {
+    await db.user.create({
+      data: { ...toBeCreatedUserData },
+    });
+  } catch (error) {
+    console.log(
+      "Could not create user.",
+      `Error: ${error}, User Data: ${toBeCreatedUserData}`,
+    );
+  }
+
   return new Response("Webhook received", { status: 200 });
 };
